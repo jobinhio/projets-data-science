5554e955a69735c003a83298c89b6ad4b3f9d097
congo <jobcongo2@gmail.com>
2024-04-12 12:26:33 +0200

    up

--- src/functions/linear_programming.py
+++ src/functions/linear_programming.py
@@ -3,8 +3,8 @@ from scipy.optimize import linprog
 def solve_linear_program(C, A_ub, b_ub, A_eq, b_eq,bounds):
     # Résoudre le problème d'optimisation linéaire
     # res = linprog(C, A_ub=A_ub, b_ub=b_ub, A_eq=A_eq, b_eq=b_eq, bounds=bounds, method ='interior-point',callback=linprog_terse_callback, options={'disp': True,'tol':0.0001})
-    res = linprog(C, A_ub=A_ub, b_ub=b_ub, A_eq=A_eq, b_eq=b_eq, bounds=bounds, method ='interior-point')
-    res = linprog(C, A_ub=A_ub, b_ub=b_ub, A_eq=A_eq, b_eq=b_eq,bounds=bounds,  method ='highs-ipm')
+    res = linprog(C, A_ub=A_ub, b_ub=b_ub, A_eq=A_eq, b_eq=b_eq, bounds=bounds, method ='highs-ipm')
+    res = linprog(C, A_ub=A_ub, b_ub=b_ub, A_eq=A_eq, b_eq=b_eq,bounds=bounds,  method ='interior-point')
 
     # Affichage du résultat
     print("Message :", res.message)
