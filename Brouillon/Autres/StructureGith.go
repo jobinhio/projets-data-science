b67fc4d11d163d03e305fe5b584728f26838c14e
congo <jobcongo2@gmail.com>
2024-06-21 13:02:03 +0200

    add brouillon

---
+++ Brouillon/Autres/StructureGith.go
@@ -0,0 +1,53 @@
+project/
+│
+├── Statistique/
+│   ├── data/
+│   │   ├── données brut/
+│   │   │   ├── Traction2023.xlsx
+│   │   │   └── Traction2024.xlsx
+│   │   │      
+│   │   ├── données nétoyées/
+│   │   │   └── Traction.xlsx
+│   │   └── GS/
+│   │       ├── GS 450-10.xlsx
+│   │       └── GS 400-15.xlsx  
+│   │           
+│   ├── notebooks/
+│   │   ├── GS 450-10.ipynb
+│   │   ├── GS 400-15.ipynb
+│   │   ├── GS 600-3.ipynb
+│   │   └── GS 600-3.ipynb
+│   └── functions/
+│		├── __init__.py
+│		├── constants.py
+│		├── correlation.py
+│		├── linear_regression.py
+│		├── outliers.py
+│		└── preprocessing.py
+│
+├── Optimisation/
+│   └── src/
+│       ├── data/
+│		│	├── recipe_optimization_data.xlsm
+│       │   └── Resultats.xlsx
+│       ├── functions/
+│       │   ├── __init__.py
+│       │   ├── cleaning.py
+│       │   ├── constraints.py
+│       │   ├── excel.py
+│       │   ├── linear_programming.py
+│       │   ├── data_processing.py
+│       │   └── result_processing.py
+│       └── main.py
+│
+├── Rapport_de_stage/
+│   ├── Rapport.pdf
+│   ├── Présentation.tex
+│   └── Autres_documents/
+│
+├── .gitignore
+├── README.md
+└── requirements.txt
+
+
+
